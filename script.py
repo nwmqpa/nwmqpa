e4b2e2cef09196b3c662e21b27a1dd79cfd7904f
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-01-14 14:06:33 +0100

    :sparkles: Added trilium to the list of app the script is tracking.

--- script.py
+++ script.py
@@ -15,6 +15,18 @@ def get_visual_studio_code_versions():
     tags = sorted(tags)
     return [str(tag) for tag in tags]
 
+def get_trilium_versions():
+    tags = subprocess.check_output(["git", "ls-remote", "--tags", "git@github.com:zadam/trilium"]).decode()
+    tags = filter(len, tags.split("\n"))
+    tags = map(lambda x: x.split("\t")[1], tags)
+    tags = map(lambda x: re.search(r"^refs/tags/v(([0-9]+.?)+)$", x), tags)
+    tags = filter(lambda x: x is not None, tags)
+    tags = map(lambda x: x.groups()[0], tags)
+    tags = map(lambda x: version.parse(x), tags)
+    tags = filter(lambda x: x >= version.parse("0.28.2") and x not in [version.parse("0.30.6"), version.parse("0.37.7")], tags)
+    tags = sorted(tags)
+    return [str(tag) for tag in tags]
+
 def get_available_versions(package: str) -> [str]:
     """Return the avaible versions for the given package in the repository."""
     regex = re.compile(package.split("/")[-1] + "-(.*)\.ebuild$")
@@ -35,7 +47,7 @@ def update(package: str):
     for version in to_add_versions:
         os.system("cp {0}/ebuild.sample {0}/{1}-{2}.ebuild".format(package, package.split("/")[-1], version))
         os.system("cd {0} && sudo ebuild {1}-{2}.ebuild manifest".format(package, package.split("/")[-1], version))
-    os.system("git add {} && git commit -m \"{0}: Added v{1}\"".format(package, package.split("/")[-1], ", v".join(to_add_versions)))
+    os.system("git add {0} && git commit -m \"{1}: Added v{2}\"".format(package, package.split("/")[-1], ", v".join(to_add_versions)))
 
 
 def main():
